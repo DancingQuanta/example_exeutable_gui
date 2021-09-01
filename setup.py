de9428a93a3b7ba323776f3bc1a603837ad0e8eb
DancingQuanta <andytheseeker@gmail.com>
2021-09-01 15:17:17 +0100

    Update setup.py with versioneer and entry points

--- setup.py
+++ setup.py
@@ -1,30 +1,53 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
 from setuptools import setup
 
-requirements = [
-    # TODO: put your package requirements here
-]
+import versioneer
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding="utf-8") as fd:
+        return re.sub(text_type(r":[a-z]+:`~?(.*?)`"), text_type(r"``\1``"), fd.read())
 
 setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     name='example_executable_gui',
-    version='0.0.1',
-    description="A PyQt5 GUI application that displays a message depending how it is launched",
+    url='https://github.com/DancingQuanta/example_executable_gui',
     author="Andrew Tolmie",
     author_email='andytheseeker@gmail.com',
-    url='https://github.com/DancingQuanta/example_executable_gui',
-    packages=['toy_gui', 'toy_gui.images',
-              'toy_gui.tests'],
+    description="A PyQt5 GUI application that displays a message depending how it is launched",
+    long_description=read("README.rst"),
+    packages=['toy_gui', 'toy_gui.images', 'toy_gui.tests'],
     package_data={'toy_gui.images': ['*.png']},
+    install_requires=[
+        r for r in open("requirements.txt").read().splitlines()
+        if r and not re.match(r"\s*\#", r)
+    ],
+    extras_require={
+        "dev": [
+            r for r in open("requirementsDev.txt").read().splitlines()
+            if r and not re.match(r"\s*\#", r)
+        ]
+    },
     entry_points={
         'console_scripts': [
             'ToyGUI=toy_gui.toy_gui:main'
+        ],
+        'gui_scripts': [
+            'ToyGUI=toy_gui.toy_gui:gui_main'
         ]
     },
-    install_requires=requirements,
-    zip_safe=False,
-    keywords='example_executable_gui',
     classifiers=[
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
